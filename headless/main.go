66d8cca4f1833f1b37c5fe2a0bc15ee3f0600eb5
WestEast1st <bhns1270132@gn.iwasaki.ac.jp>
2019-05-20 14:06:20 +0900

    EventJavascriptDialogOpeningの追加

--- headless/main.go
+++ headless/main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"io/ioutil"
 	"log"
-	"os"
 	"time"
 
+	"github.com/chromedp/cdproto/page"
+	"github.com/chromedp/chromedp"
 	chr "github.com/chromedp/chromedp"
 )
 
@@ -15,23 +18,40 @@ func main() {
 		chr.WithLogf(log.Printf),
 	)
 	defer cancel()
-	ctx, cancel = context.WithTimeout(ctx, 15*time.Second)
+	ctx, cancel = context.WithTimeout(ctx, 5*time.Second)
 	defer cancel()
 
 	go func() {
-		for index := 0; index < 15; index++ {
+		for index := 0; index < 5; index++ {
 			log.Println(index + 1)
 			time.Sleep(1 * time.Second)
 		}
 	}()
 	showURL := "http://localhost/xss/test.php"
 	var res string
+	var buf []byte
+	chromedp.ListenTarget(ctx, func(ev interface{}) {
+		if ev, ok := ev.(*page.EventJavascriptDialogOpening); ok {
+			fmt.Println("closing alert:", ev.Message)
+			go func() {
+				if err := chromedp.Run(ctx,
+					page.HandleJavaScriptDialog(true),
+				); err != nil {
+					panic(err)
+				}
+			}()
+		}
+	})
 	err := chr.Run(ctx, chr.Tasks{
 		chr.Navigate(showURL),
+		chr.CaptureScreenshot(&buf),
 		chr.OuterHTML("html", &res),
 	})
 	if err != nil {
-		os.Exit(-1)
+		fmt.Println(buf)
+		log.Fatal(err)
+	}
+	if err = ioutil.WriteFile("screenshot.png", buf, 0644); err != nil {
 		log.Fatal(err)
 	}
 	log.Printf("%s", res)
